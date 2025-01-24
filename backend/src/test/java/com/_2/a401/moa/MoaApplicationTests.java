0aadc33ddbdcd3197a782a376cf4acccd19a35bb
최성환 <aiya0422@gmail.com>
2025-01-24 17:42:10 +0900

    chore: 프로젝트 생성

---
+++ backend/src/test/java/com/_2/a401/moa/MoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com._2.a401.moa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
