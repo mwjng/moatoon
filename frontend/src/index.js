179df3bb3fd196cdb405f76a246de43fd757ba3e
최성환 <aiya0422@gmail.com>
2025-02-03 17:29:45 +0900

    feat: 네비게이션 구현 중

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import DrawPage from './pages/drawPage';
+import Navigation from './components/Navigation';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<App />} />
+            <Route path="/session">
+                <Route path="draw" element={<DrawPage />} />
+                <Route path="search" />
+                <Route path="create" />
+            </Route>
+            <Route path="library">
+                <Route index />
+            </Route>
+            <Route path="word">
+                <Route index />
+            </Route>
+            <Route path="user">
+                <Route index />
+            </Route>
+            <Route path="/nav" element={<Navigation />} />
+        </Routes>
+    </BrowserRouter>,
 );
