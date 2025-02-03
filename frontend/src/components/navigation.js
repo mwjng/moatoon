2fb71258e4fefd91d2677fcce3970b2440a65ab7
최성환 <aiya0422@gmail.com>
2025-02-03 10:31:35 +0900

    fix: eslint 삭제

---
+++ frontend/src/components/navigation.js
@@ -0,0 +1,19 @@
+function Navigation() {
+    return (
+        <header className="App-header">
+            <div className="flex flex-row">
+                <div>아이콘</div>
+                <div>홈</div>
+                <div>도서관</div>
+                <div>방 탐색</div>
+                <div>방 생성</div>
+                <div>회원정보 수정</div>
+            </div>
+            <div>
+                <button>로그아웃</button>
+            </div>
+        </header>
+    );
+}
+
+export default Navigation;
