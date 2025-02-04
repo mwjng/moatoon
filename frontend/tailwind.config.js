e39d6bed8e0e593d061ae3e3518d946ba748fb50
김소현 <ksh96611@naver.com>
2025-02-04 13:12:50 +0900

    feat: 랜딩페이지 구현 (S12P11A401-211)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        scrollLeft: "scrollLeft 50s linear infinite",
+        scrollRight: "scrollRight 50s linear infinite",
+      },
+      keyframes: {
+        scrollLeft: {
+          "from": { transform: "translateX(0%)" },
+          "to": { transform: "translateX(-85.8%)" },
+        },
+        scrollRight: {
+          "from": { transform: "translateX(-85.8%)" },
+          "to": { transform: "translateX(0%)" },
+        },
+      },
+    },
   },
   plugins: [
     require('daisyui'),
