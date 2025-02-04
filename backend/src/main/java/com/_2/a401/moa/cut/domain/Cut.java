0637d5794429fbfdadab766854e8c687ee5b587c
이준영 <uhiccup@gmail.com>
2025-02-04 16:15:32 +0900

    fix: Cut의 status 제거 -> Schedule에 status 추가

--- backend/src/main/java/com/_2/a401/moa/cut/domain/Cut.java
+++ backend/src/main/java/com/_2/a401/moa/cut/domain/Cut.java
@@ -3,6 +3,7 @@ package com._2.a401.moa.cut.domain;
 import com._2.a401.moa.common.auditing.BaseEntity;
 import com._2.a401.moa.member.domain.Member;
 import com._2.a401.moa.party.domain.Party;
+import com._2.a401.moa.schedule.domain.ScheduleState;
 import com._2.a401.moa.word.domain.Word;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -27,10 +28,6 @@ public class Cut extends BaseEntity {
     private int cutOrder;
 
     private int randomOrder;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private CutState status;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
---
+++ backend/src/main/java/com/_2/a401/moa/schedule/domain/ScheduleState.java
@@ -0,0 +1,5 @@
+package com._2.a401.moa.schedule.domain;
+
+public enum ScheduleState {
+    DONE, ONGOING
+}
