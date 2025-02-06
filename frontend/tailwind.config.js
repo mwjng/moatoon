94d7ff536d71ce9d323b32766cd7dc9c6e57af7f
이준영 <uhiccup@gmail.com>
2025-02-06 23:54:42 +0900

    feat: 아동 메인 화면 UI 구현 (S12P11A401-265)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,6 +18,12 @@ module.exports = {
             },
             colors: {
                 'custom-blue': '#CAE0F4', // 여기에 커스텀 색상을 추가
+                'orange1': '#FE9134',
+                'orange2': '#FC852B', // 좀 더 진한 오렌지
+                'light-cream': '#FDFFE9', // 아동 메인 배경색
+                'butter-cream' : '#FFF8CF',
+                'lime-cream' : '#F3F8C0',
+                'gray1' : '#444444'
             },
         },
     },
