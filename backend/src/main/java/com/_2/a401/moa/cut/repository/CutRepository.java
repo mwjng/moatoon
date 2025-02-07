924f41bdcacccff0dbe2f75ae1a63a41fccdb0fb
김유민 <march02198@gmail.com>
2025-02-07 11:06:35 +0900

    feat: 그림 (최종) 저장 api 구현

--- backend/src/main/java/com/_2/a401/moa/cut/repository/CutRepository.java
+++ backend/src/main/java/com/_2/a401/moa/cut/repository/CutRepository.java
@@ -2,6 +2,13 @@ package com._2.a401.moa.cut.repository;
 
 import com._2.a401.moa.cut.domain.Cut;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CutRepository extends JpaRepository<Cut, Long> {
+
+    @Modifying
+    @Query(value = "UPDATE Cut c SET c.image_url=:cutFileUrl WHERE c.id=:cutId"
+    , nativeQuery = true)
+    void savePictureByCutId(String cutFileUrl, Long cutId);
 }
