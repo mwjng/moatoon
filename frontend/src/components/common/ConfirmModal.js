3bf6c7765091db9ac2570feccd15fececbb2080e
김소현 <ksh96611@naver.com>
2025-02-08 18:28:40 +0900

    fix: confirmModal state 추가

--- frontend/src/components/common/ConfirmModal.js
+++ frontend/src/components/common/ConfirmModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function ConfirmModal(props) {
+    if (!props.modalState) return null;
     return (
         <div className="absolute top-0 left-0 w-full h-full bg-[#00000088] z-3">
             <div
