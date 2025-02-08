7fb63fff1fbf4a88362f3d0aad865a072a608970
김소현 <ksh96611@naver.com>
2025-02-08 17:35:49 +0900

    feat: 로그인페이지(S12P11A401-212,S12P11A401-203)

--- backend/src/main/java/com/_2/a401/moa/member/service/MemberService.java
+++ backend/src/main/java/com/_2/a401/moa/member/service/MemberService.java
@@ -1,7 +1,10 @@
 package com._2.a401.moa.member.service;
 
+import com._2.a401.moa.member.dto.request.MemberCreate;
 import com._2.a401.moa.member.repository.MemberRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void createMember(@Valid MemberCreate memberCreate) {
+        memberRepository.save(memberCreate.toMember(passwordEncoder));
+    }
 }
--- frontend/src/components/member/BackgroundRendingBook.js
+++ frontend/src/components/member/BackgroundRendingBook.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import book1 from '../../assets/images/book1.png';
 import book2 from '../../assets/images/book2.png';
 import book3 from '../../assets/images/book3.png';
@@ -10,27 +9,25 @@ import book7 from '../../assets/images/book7.png';
 
 const bookImages = [book1, book2, book3, book4, book5, book6, book7];
 
-function RendingBooks() {
+export default function BackgroundRendingBook() {
     return (
         <div
-            className="relative w-full bg-yellow-50 overflow-hidden"
-            style={{ transform: 'rotate(-25deg)', height: '400px' }}
+            className="relative w-full bg-yellow-50 overflow-hidden "
+            style={{ transform: 'rotate(-25deg)', height: '400px', backgroundColor: '#FEFBEB' }}
         >
             {/* 첫 번째 줄 (왼쪽 → 오른쪽) */}
-            <div className="absolute top-0 w-full flex gap-4 animate-scrollLeft w-[200%]">
-                {[...bookImages, ...bookImages, ...bookImages].map((src, index) => (
+            <div className="absolute top-0 w-full flex gap-4  w-[200%] left-20">
+                {[...bookImages, ...bookImages].map((src, index) => (
                     <img key={index} src={src} className="w-40 h-48 object-cover shrink-0" alt="book" />
                 ))}
             </div>
 
             {/* 두 번째 줄 (오른쪽 → 왼쪽) */}
-            <div className="absolute bottom-0 w-full flex gap-4 animate-scrollRight w-[200%]">
-                {[...bookImages, ...bookImages, ...bookImages].map((src, index) => (
+            <div className="absolute bottom-0 w-full flex gap-4  w-[200%] right-[50%]">
+                {[...bookImages, ...bookImages].map((src, index) => (
                     <img key={index} src={src} className="w-40 h-48 object-cover shrink-0" alt="book" />
                 ))}
             </div>
         </div>
     );
 }
-
-export default RendingBooks;
