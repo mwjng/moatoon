f64569474732b97c60629604603da9f32b34651c
정민우 <wmw04087@gmail.com>
2025-02-09 15:31:47 +0900

    feat: 서버 세션 구현 (S12P11A401-236)

--- backend/src/main/java/com/_2/a401/moa/party/repository/PartyMemberRepository.java
+++ backend/src/main/java/com/_2/a401/moa/party/repository/PartyMemberRepository.java
@@ -2,6 +2,13 @@ package com._2.a401.moa.party.repository;
 
 import com._2.a401.moa.party.domain.PartyMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PartyMemberRepository extends JpaRepository<PartyMember, Long> {
+
+    @Query(value = "SELECT member_id FROM party_member WHERE party_id = :partyId", nativeQuery = true)
+    List<Long> findMemberIdsByPartyId(@Param("partyId") Long partyId);
 }
