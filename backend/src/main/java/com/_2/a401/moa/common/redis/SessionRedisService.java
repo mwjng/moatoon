8e5e3b8a409bda05f81c8b144adcaa1dc30b5999
이준영 <uhiccup@gmail.com>
2025-02-10 01:21:02 +0900

    fix: SessionRedisService 로직 수정
    -

--- backend/src/main/java/com/_2/a401/moa/common/redis/SessionRedisService.java
+++ backend/src/main/java/com/_2/a401/moa/common/redis/SessionRedisService.java
@@ -15,13 +15,13 @@ import java.util.Map;
 public class SessionRedisService {
     private final RedisService redisService;
 
-    public void saveSession(Long sessionId) {
+    public void saveSession(Long scheduleId) {
         // Session Info 저장
         RedisSessionInfo sessionInfo = RedisSessionInfo.builder()
-                .sessionId(sessionId)
+                .sessionId(1L) // TODO: sessionId도 알아야함
                 .sessionStage(SessionStage.BEFORE)
                 .build();
-        redisService.set(RedisKeyConfig.getSessionInfoKey(sessionId), sessionInfo);
+        redisService.set(RedisKeyConfig.getSessionInfoKey(scheduleId), sessionInfo);
 
         // Session Member Status 저장
         Map<Long, RedisMemberStatus> memberStatuses = new HashMap<>();
@@ -36,43 +36,86 @@ public class SessionRedisService {
             memberStatuses.put(memberId, status);
         }
 
-        redisService.set(RedisKeyConfig.getSessionMemberKey(sessionId), memberStatuses);
+        redisService.set(RedisKeyConfig.getSessionMemberKey(scheduleId), memberStatuses);
     }
 
     // 세션 정보 조회
-    public RedisSessionInfo getSessionInfo(Long sessionId) {
-        saveSession(sessionId);
-        return redisService.get(RedisKeyConfig.getSessionInfoKey(sessionId), new TypeReference<RedisSessionInfo>() {});
+    public RedisSessionInfo getSessionInfo(Long scheduleId) {
+        saveSession(scheduleId);
+        return redisService.get(RedisKeyConfig.getSessionInfoKey(scheduleId), new TypeReference<RedisSessionInfo>() {});
     }
 
     // 세션 멤버 상태 조회
-    public Map<Long, RedisMemberStatus> getSessionMemberStatuses(Long sessionId) {
-        return redisService.get(RedisKeyConfig.getSessionMemberKey(sessionId),
+    public Map<Long, RedisMemberStatus> getSessionMemberStatuses(Long scheduleId) {
+        return redisService.get(RedisKeyConfig.getSessionMemberKey(scheduleId),
                 new TypeReference<Map<Long, RedisMemberStatus>>() {});
     }
 
     // Redis에 Map 저장을 위한 메서드 (RedisTemplate 사용 예시)
-    public void setSessionMemberStatuses(Long sessionId, Map<Long, RedisMemberStatus> memberStatuses) {
-        redisService.set(RedisKeyConfig.getSessionMemberKey(sessionId), memberStatuses);
+    public void setSessionMemberStatuses(Long scheduleId, Map<Long, RedisMemberStatus> memberStatuses) {
+        redisService.set(RedisKeyConfig.getSessionMemberKey(scheduleId), memberStatuses);
     }
 
-    // 특정 멤버 상태 업데이트
-    public void updateMemberStatus(Long sessionId, Long memberId, RedisMemberStatus newStatus) {
-        String key = RedisKeyConfig.getSessionMemberKey(sessionId);
-        Map<Long, RedisMemberStatus> currentStatuses = getSessionMemberStatuses(sessionId);
+    // 특정 멤버의 ready상태 업데이트
+    public void changeReadyStatus(Long scheduleId, Long memberId, boolean isReady) {
+        String key = RedisKeyConfig.getSessionMemberKey(scheduleId);
+        Map<Long, RedisMemberStatus> currentStatuses = getSessionMemberStatuses(scheduleId);
 
         if (currentStatuses != null) {
-            currentStatuses.put(memberId, newStatus);
-            redisService.set(key, currentStatuses);
+            RedisMemberStatus currentStatus = currentStatuses.get(memberId);
+
+            if (currentStatus != null) {
+                // 기존 상태에서 isReady만 업데이트
+                RedisMemberStatus updatedStatus = new RedisMemberStatus(
+                        isReady,  // 새로운 isReady 값
+                        currentStatus.isConnect()  // 기존 isConnect 값 유지
+                );
+
+                currentStatuses.put(memberId, updatedStatus);
+                redisService.set(key, currentStatuses);
+            }
         }
     }
 
+    // 특정 멤버의 connect 상태 업데이트
+    public void changeConnectStatus(Long scheduleId, Long memberId, boolean isConnect) {
+        String key = RedisKeyConfig.getSessionMemberKey(scheduleId);
+        Map<Long, RedisMemberStatus> currentStatuses = getSessionMemberStatuses(scheduleId);
+
+        if (currentStatuses != null) {
+            RedisMemberStatus currentStatus = currentStatuses.get(memberId);
+
+            if (currentStatus != null) {
+                // 연결 끊길 경우, isReady도 false로 설정
+                if (!isConnect) {
+                    RedisMemberStatus updatedStatus = new RedisMemberStatus(
+                            false,
+                            false
+                    );
+
+                    currentStatuses.put(memberId, updatedStatus);
+                } else {
+                    // 연결이 되면 isConnect만 업데이트
+                    RedisMemberStatus updatedStatus = new RedisMemberStatus(
+                            isConnect,  // 새로운 isConnect 값
+                            currentStatus.isReady()  // 기존 isReady 값 유지
+                    );
+
+                    currentStatuses.put(memberId, updatedStatus);
+                }
+                redisService.set(key, currentStatuses);
+            }
+        }
+    }
+
+
     // 세션 상태 업데이트
-    public void updateSessionStage(Long sessionId, SessionStage newStage) {
-        String key = RedisKeyConfig.getSessionInfoKey(sessionId);
-        RedisSessionInfo currentInfo = getSessionInfo(sessionId);
+    public void updateSessionStage(Long scheduleId, SessionStage newStage) {
+        String key = RedisKeyConfig.getSessionInfoKey(scheduleId);
+        RedisSessionInfo currentInfo = getSessionInfo(scheduleId);
 
         if (currentInfo != null) {
+            // RedisSessionInfo를 불변으로 만들어서 새로 생성해서 넣어준다.
             RedisSessionInfo updatedInfo = RedisSessionInfo.builder()
                     .sessionId(currentInfo.sessionId())
                     .sessionStage(newStage)
@@ -81,20 +124,21 @@ public class SessionRedisService {
         }
     }
 
-    // 모든 멤버가 ready 상태인지 확인
-    public boolean isAllMembersReady(Long sessionId) {
-        Map<Long, RedisMemberStatus> memberStatuses = getSessionMemberStatuses(sessionId);
+    // 모든 연결된 멤버가 ready 상태인지 확인
+    public boolean isAllMembersReady(Long scheduleId) {
+        Map<Long, RedisMemberStatus> memberStatuses = getSessionMemberStatuses(scheduleId);
         if (memberStatuses == null || memberStatuses.isEmpty()) {
             return false;
         }
 
         return memberStatuses.values().stream()
-                .allMatch(RedisMemberStatus::isReady);
+                .filter(RedisMemberStatus::isConnect) // 연결된 멤버만 필터링
+                .allMatch(RedisMemberStatus::isReady); // 연결된 멤버들이 모두 준비 완료인지 확인
     }
 
     // 세션 삭제
-    public void deleteSession(Long sessionId) {
-        redisService.delete(RedisKeyConfig.getSessionInfoKey(sessionId));
-        redisService.delete(RedisKeyConfig.getSessionMemberKey(sessionId));
+    public void deleteSession(Long scheduleId) {
+        redisService.delete(RedisKeyConfig.getSessionInfoKey(scheduleId));
+        redisService.delete(RedisKeyConfig.getSessionMemberKey(scheduleId));
     }
 }
