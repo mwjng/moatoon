ca94fad2e5aff9b29e0e1dabc80194c13513a767
이준영 <uhiccup@gmail.com>
2025-02-10 18:19:33 +0900

    feat: 아동 일정 조회 api 연결 (S12P11A401-271, S12P11A401-335)

--- backend/src/main/java/com/_2/a401/moa/schedule/dto/response/UpcomingSchedule.java
+++ backend/src/main/java/com/_2/a401/moa/schedule/dto/response/UpcomingSchedule.java
@@ -3,17 +3,21 @@ package com._2.a401.moa.schedule.dto.response;
 import com._2.a401.moa.schedule.dto.ScheduleInfo;
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 @Builder
 public record UpcomingSchedule(
         Long scheduleId,
         String bookTitle,
-        String bookCover
+        String bookCover,
+        LocalDateTime sessionTime
 ) {
     public static UpcomingSchedule from(ScheduleInfo schedule) {
         return UpcomingSchedule.builder()
                 .scheduleId(schedule.scheduleId())
                 .bookTitle(schedule.bookTitle())
                 .bookCover(schedule.bookCover())
+                .sessionTime(schedule.getSessionTimeAsLocalDateTime())
                 .build();
     }
 }
