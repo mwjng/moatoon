72c73589ad940c52aa7d5ca145204672e78d3037
정민우 <wmw04087@gmail.com>
2025-02-10 10:58:25 +0900

    feat: 세션 상태 관리 구현(S12P11A401-386)

---
+++ backend/src/main/java/com/_2/a401/moa/schedule/repository/SessionMemberRepository.java
@@ -0,0 +1,15 @@
+package com._2.a401.moa.schedule.repository;
+
+import com._2.a401.moa.common.exception.MoaException;
+import com._2.a401.moa.schedule.domain.SessionMember;
+import org.springframework.data.repository.CrudRepository;
+
+import static com._2.a401.moa.common.exception.ExceptionCode.SESSION_NOT_FOUND;
+
+public interface SessionMemberRepository extends CrudRepository<SessionMember, Long> {
+
+    default SessionMember fetchByScheduleId(Long scheduleId) {
+        return findById(scheduleId)
+            .orElseThrow(() -> new MoaException(SESSION_NOT_FOUND));
+    }
+}
