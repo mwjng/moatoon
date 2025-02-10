5bf9f31a955c378492af5a1d9d1a867ac40fce46
최성환 <aiya0422@gmail.com>
2025-02-10 21:08:33 +0900

    feat: 더미 데이터를 스켈레톤으로 수정

--- frontend/src/pages/WordLearning.js
+++ frontend/src/pages/WordLearning.js
@@ -4,38 +4,36 @@ import WordInfo from '../components/word/WordInfo';
 import CheckIcon from '../assets/icon-check.png';
 import { getLearningWords } from '../api/word';
 
-const WordLearning = () => {
+const WordLearning = ({ partyId }) => {
     const [words, setWords] = useState([
         {
-            wordId: 5,
-            word: '글씨',
-            meaning: '쓴 글자의 모양.',
-            examples: ['*글씨*를 또박또박 쓰세요.', '내 이름을 *글씨*로 써 보세요.'],
+            wordId: 1,
+            word: 'word',
+            meaning: 'meaning',
+            examples: ['example1', 'example2'],
         },
         {
             wordId: 1,
-            word: '그림',
-            meaning:
-                '선이나 색채를 써서 사물의 형상이나 이미지를 평면 위에 나타낸 것.\\n아름다운 경치를 비유적으로 이르는 말.',
-            examples: ['나는 색연필로 *그림*을 그렸다.', '벽에 예쁜 *그림*이 걸려 있어.'],
+            word: 'word',
+            meaning: 'meaning',
+            examples: ['example1', 'example2'],
         },
         {
-            wordId: 3,
-            word: '그만',
-            meaning: '그 정도까지.\\n그대로 곧.\\n자신도 모르는 사이에.',
-            examples: ['이제 *그만* 싸우자!', '장난은 *그만*하세요.'],
+            wordId: 1,
+            word: 'word',
+            meaning: 'meaning',
+            examples: ['example1', 'example2'],
         },
         {
-            wordId: 7,
-            word: '글자',
-            meaning: '말을 적는 일정한 체계의 부호.',
-            examples: ['한글은 소중한 *글자*예요.', '*글자*를 바르게 배워야 해.'],
+            wordId: 1,
+            word: 'word',
+            meaning: 'meaning',
+            examples: ['example1', 'example2'],
         },
     ]);
     const [currentWordIdx, setCurrentWordIdx] = useState(0);
     const bgColors = ['bg-[#FFFFFF]', 'bg-[#FDFCDC]', 'bg-[#FED9B7]', 'bg-[#FFB5A7]'];
     const [checkedWords, setCheckedWords] = useState(new Set());
-    const [partyId, setPartyId] = useState(1); //더미 값
 
     const handleCheck = wordId => {
         setCheckedWords(prev => {
