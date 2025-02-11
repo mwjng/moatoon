937ad52adeea27049b196b83e0c6f30a088fca6a
wmw04087 <wmw04087@gmail.com>
2025-02-11 15:52:54 +0900

    feat: 세션 종료 api 요청 함수 작성

--- frontend/src/api/room.js
+++ frontend/src/api/room.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const APPLICATION_SERVER_URL = 'http://localhost:8080/schedule';
+const APPLICATION_SERVER_URL = 'http://localhost:8080/schedules';
 
 export const getSessionToken = async (scheduleId, token) => {
     try {
@@ -14,6 +14,22 @@ export const getSessionToken = async (scheduleId, token) => {
         return response.data;
     } catch (error) {
         console.error('토큰 발급 에러', error);
+        throw error;
+    }
+};
+
+export const closeSession = async (scheduleId, token) => {
+    try {
+        const response = await axios.delete(
+            `${APPLICATION_SERVER_URL}/${scheduleId}/session/leave`,
+            {},
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            },
+        );
+        return response;
+    } catch (error) {
+        console.error('세션 종료 에러', error);
         throw error;
     }
 };
