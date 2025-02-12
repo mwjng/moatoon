502f8ba39b17d9ee8f2779d1ba26f5ba080c085d
김유민 <march02198@gmail.com>
2025-02-12 17:32:57 +0900

    fix: cutCard 이미지 props로 변경

--- frontend/src/components/CutSvgCard.js
+++ frontend/src/components/CutSvgCard.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const CutCard = ({ item }) => {
-    const img1 = 'https://moabucket.s3.amazonaws.com/9ffe6925-b398-4746-af26-3b12f2b22d58-drawing.svg';
     return (
         <div className="relative w-full overflow-hidden bg-white">
-            <img src={img1} className="object-cover w-full h-full" alt="cut" />
+            <img src={item.imageUrl} className="object-cover w-full h-full" alt="cut" />
             <div className="absolute w-full py-2.5 bottom-0 inset-x-0 text-black text-xs text-center leading-4">
                 <p
                     className="text-md text-center text-gray-700"
