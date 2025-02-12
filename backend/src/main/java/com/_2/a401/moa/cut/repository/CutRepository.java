b16187030488218275d39a0683149545d6334a11
Youmin Kim <march02198@gmail.com>
2025-02-13 06:14:44 +0900

    feat: 컷 초기 필요 정보 조회 API 구현

--- backend/src/main/java/com/_2/a401/moa/cut/repository/CutRepository.java
+++ backend/src/main/java/com/_2/a401/moa/cut/repository/CutRepository.java
@@ -1,6 +1,7 @@
 package com._2.a401.moa.cut.repository;
 
 import com._2.a401.moa.cut.domain.Cut;
+import com._2.a401.moa.cut.dto.response.CutInfoResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -20,4 +21,10 @@ public interface CutRepository extends JpaRepository<Cut, Long> {
             "AND c.cut_order BETWEEN :startRange AND :endRange", nativeQuery = true)
     List<Cut> getCutsByRange(@Param("partyId") Long partyId, @Param("startRange") int startRange, @Param("endRange") int endRange);
 
+    @Query(value = "SELECT c.id AS cutId, c.content AS content, c.cut_order AS cutOrder, " +
+            "c.member_id AS memberId, m.nickname AS nickname, c.word_id as wordId, c.party_id as partyId FROM Cut c " +
+            "JOIN member m ON m.id=c.member_id " +
+            "WHERE c.party_id = :partyId " +
+            "AND c.cut_order BETWEEN :startRange AND :endRange", nativeQuery = true)
+    List<CutInfoResponse> getCutsAndMemberByRange(@Param("partyId") Long partyId,@Param("startRange") int startRange,@Param("endRange") int endRange);
 }
