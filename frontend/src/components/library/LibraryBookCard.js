2abb200e6a569d3ebe12fa6dc01d60ec36d26f99
김유민 <march02198@gmail.com>
2025-02-14 11:11:10 +0900

    feat: 날짜 시간 제거 추가

--- frontend/src/components/library/LibraryBookCard.js
+++ frontend/src/components/library/LibraryBookCard.js
@@ -2,6 +2,12 @@ import React from 'react';
 import book from '../../assets/images/book1.png';
 
 const LibraryBookCard = ({ item }) => {
+    //날짜 시간 제거
+    const formatDate = date => {
+        const newDate = new Date(date);
+        return newDate.toISOString().split('T')[0]; // 날짜만 반환 (YYYY-MM-DD 형식)
+    };
+
     return (
         <div className="flex-shrink-0 w-full">
             <div className="overflow-hidden rounded-2xl">
@@ -10,7 +16,7 @@ const LibraryBookCard = ({ item }) => {
             <p className="mt-2 text-sm font-semibold text-gray-800 text-center truncate">{item.bookTitle}</p>
             <div className="mt-1 text-xs text-gray-500 text-center">
                 <p>
-                    {item.startDate}~{item.endDate}
+                    {formatDate(item.startDate)}~{formatDate(item.endDate)}
                 </p>
             </div>
         </div>
