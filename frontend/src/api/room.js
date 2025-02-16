d0e408c73ab496e0ca903af4f084242303d7e737
SeongHwan <aiya0422@gmail.com>
2025-02-16 19:38:19 +0900

    fix: room api의 서버 주소 수정

--- frontend/src/api/room.js
+++ frontend/src/api/room.js
@@ -1,10 +1,8 @@
 import { authInstance } from './axios';
 
-const APPLICATION_SERVER_URL = 'http://localhost:8080/schedules';
-
 export const getSessionToken = async (scheduleId, token) => {
     try {
-        const response = await authInstance.post(`${APPLICATION_SERVER_URL}/${scheduleId}/session/join`, {});
+        const response = await authInstance.post(`/schedules/${scheduleId}/session/join`, {});
         return response.data;
     } catch (error) {
         console.error('토큰 발급 에러', error);
@@ -14,7 +12,7 @@ export const getSessionToken = async (scheduleId, token) => {
 
 export const closeSession = async (scheduleId, token) => {
     try {
-        const response = await authInstance.delete(`${APPLICATION_SERVER_URL}/${scheduleId}/session/leave`, {});
+        const response = await authInstance.delete(`/schedules/${scheduleId}/session/leave`, {});
         return response;
     } catch (error) {
         console.error('세션 종료 에러', error);
@@ -22,7 +20,7 @@ export const closeSession = async (scheduleId, token) => {
     }
 };
 
-export const getEnterSession = async (pinNumber) => {
+export const getEnterSession = async pinNumber => {
     try {
         const response = await authInstance.get(`/session/pinNumber/${pinNumber}`); // ✅ 올바른 URL 확인
         return response.data;
@@ -31,4 +29,3 @@ export const getEnterSession = async (pinNumber) => {
         throw error;
     }
 };
-
