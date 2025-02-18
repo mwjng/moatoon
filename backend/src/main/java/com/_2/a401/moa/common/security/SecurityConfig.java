d0514df20c74cc1c263f3830b2c8b25f5f600112
최성환 <aiya0422@gmail.com>
2025-02-18 11:06:07 +0900

    fix: security 설정 시도

--- backend/src/main/java/com/_2/a401/moa/common/security/SecurityConfig.java
+++ backend/src/main/java/com/_2/a401/moa/common/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     @Bean
     UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://i12a401.p.ssafy.io:8443"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://i12a401.p.ssafy.io"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
