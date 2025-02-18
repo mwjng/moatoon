5c7ab3c65e25c75a668f2fa8ec5d4a3e967fa340
최성환 <aiya0422@gmail.com>
2025-02-18 15:33:46 +0900

    fix: 대기방 카메라 배치 수정

--- frontend/src/pages/session/WaitingRoom.js
+++ frontend/src/pages/session/WaitingRoom.js
@@ -80,7 +80,7 @@ function WaitingRoom({
                             <MyCamera streamManager={publisher} nickname={nickname} />
                             <CameraMicControls publisher={publisher} />
                         </div>
-                        <div className="flex flex-row mt-4 grid grid-rows-3 gap-8 content-evenly mx-auto">
+                        <div className="flex flex-row mt-4 gap-8 content-evenly mx-auto">
                             {subscribers.map((subscriber, index) => (
                                 <SubscriberVideo key={index} streamManager={subscriber} />
                             ))}
