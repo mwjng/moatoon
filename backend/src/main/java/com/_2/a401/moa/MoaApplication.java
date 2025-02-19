4b1acb5527679d09a54f2b130cd12bef43bffac7
최성환 <aiya0422@gmail.com>
2025-02-19 10:12:17 +0900

    fix: 시간대 설정 시도

--- backend/src/main/java/com/_2/a401/moa/MoaApplication.java
+++ backend/src/main/java/com/_2/a401/moa/MoaApplication.java
@@ -1,13 +1,23 @@
 package com._2.a401.moa;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class MoaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoaApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println(">>> 현재 시간: {}" + LocalDateTime.now());
 	}
 
 }
