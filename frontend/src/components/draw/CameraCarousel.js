b7f7474e609fef4de4065ea27151391d79fd9125
김소현 <ksh96611@naver.com>
2025-02-21 04:47:39 +0900

    fix : 내컷 소리 해결(5차)

--- frontend/src/components/draw/CameraCarousel.js
+++ frontend/src/components/draw/CameraCarousel.js
@@ -21,11 +21,16 @@ function CameraCarousel({ publisher, subscribers, nickname }) {
             <button onClick={prevCamera} className="absolute left-0 bg-gray-300 p-2 rounded-full z-[3]">
                 <IoIosArrowBack className="h-6 w-6" />
             </button>
-            {isMyCamera ? (
+            <div className={`absolute ${isMyCamera ? '' : 'invisible'}`}>
                 <MyCamera streamManager={publisher} nickname={nickname} />
-            ) : (
-                subscribers.length > 0 && <SubscriberVideo streamManager={subscribers[currentIndex - 1]} />
-            )}
+            </div>
+
+            {subscribers.length > 0 &&
+                subscribers.map((subscriber, index) => (
+                    <div key={subscriber.id} className={`absolute ${index === currentIndex - 1 ? '' : 'invisible'}`}>
+                        <SubscriberVideo streamManager={subscriber} />
+                    </div>
+                ))}
             <button onClick={nextCamera} className="absolute right-0 bg-gray-300 p-2 rounded-full z-[3]">
                 <IoIosArrowForward className="h-6 w-6" />
             </button>
